1a46f99910de44bef4bf2ac5954d2dcf3232c43d
asilvainnovations <132311723+asilvainnovations@users.noreply.github.com>
2026-02-02 12:38:29 +0800

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { HelmetProvider } from 'react-helmet-async'
+import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
-  </React.StrictMode>,
-)
+// Remove dark mode class addition
+createRoot(document.getElementById("root")!).render(<App />);
